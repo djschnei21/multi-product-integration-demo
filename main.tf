aea20f6f470f47ffc16fb5f911c9bb4b01e6daef
djschnei21 <32646250+djschnei21@users.noreply.github.com>
2023-06-08 10:58:45 -0400

    public consul and connect enabled

--- main.tf
+++ main.tf
@@ -30,9 +30,11 @@ resource "hcp_vault_cluster" "hashistack" {
 }
 
 resource "hcp_consul_cluster" "hashistack" {
-  cluster_id = "consul-cluster"
-  hvn_id     = hcp_hvn.hashistack.hvn_id
-  tier       = "development"
+  cluster_id      = "consul-cluster"
+  hvn_id          = hcp_hvn.hashistack.hvn_id
+  tier            = "development"
+  public_endpoint = true
+  connect_enabled = true
 }
 
 resource "hcp_boundary_cluster" "hashistack" {
