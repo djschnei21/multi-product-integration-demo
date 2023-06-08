fc47b8089df338196f3862a3cc4ee3609c52144c
djschnei21 <32646250+djschnei21@users.noreply.github.com>
2023-06-08 12:23:00 -0400

    create ns

--- vault.tf
+++ vault.tf
@@ -1,8 +1,8 @@
-# provider "vault" {
-#   address = hcp_vault_cluster.hashistack.public_endpoint
-#   token   = hcp_vault_cluster_admin_token.hashistack.token
-# }
+provider "vault" {
+  address = hcp_vault_cluster.hashistack.public_endpoint
+  token   = hcp_vault_cluster_admin_token.hashistack.token
+}
 
-# resource "vault_namespace" "hashistack" {
-#   path = "hashistack"
-# }
+resource "vault_namespace" "hashistack" {
+  path = "hashistack"
+}
