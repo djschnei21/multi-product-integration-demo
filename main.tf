e868d39d6564daa9ab5cd7223b769c90604bea4d
djschnei21 <32646250+djschnei21@users.noreply.github.com>
2023-06-08 10:38:14 -0400

    create HVN

--- main.tf
+++ main.tf
@@ -4,17 +4,13 @@ terraform {
       source = "hashicorp/hcp"
       version = "0.60.0"
     }
-    doormat = {
-      source  = "doormat.hashicorp.services/hashicorp-security/doormat"
-      version = "~> 0.0.2"
-    }
   }
 }
 
-provider "doormat" {}
-
-data "doormat_aws_credentials" "creds" {
-  provider = doormat
-  role_arn = "arn:aws:iam::365006510262:role/tfc-doormat-role"
+resource "hcp_hvn" "hashistack" {
+  hvn_id         = "hashistack-hvn"
+  cloud_provider = "aws"
+  region         = "us-east-1"
+  cidr_block     = "172.25.16.0/20"
 }
 
