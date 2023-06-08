30c6a87fcce460b2ece11cade92c8de21180f9e6
djschnei21 <32646250+djschnei21@users.noreply.github.com>
2023-06-08 12:25:38 -0400

    skip_child_token

--- vault.tf
+++ vault.tf
@@ -1,6 +1,7 @@
 provider "vault" {
-  address = hcp_vault_cluster.hashistack.public_endpoint
-  token   = hcp_vault_cluster_admin_token.hashistack.token
+  address          = hcp_vault_cluster.hashistack.public_endpoint
+  token            = hcp_vault_cluster_admin_token.hashistack.token
+  skip_child_token = true
 }
 
 resource "vault_namespace" "hashistack" {
