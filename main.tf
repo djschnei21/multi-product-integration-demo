529e382ab8d1d58e3b8c3ca1cda91abaf411e473
djschnei21 <32646250+djschnei21@users.noreply.github.com>
2023-06-08 10:31:26 -0400

    doormat

--- main.tf
+++ main.tf
@@ -1,1 +1,20 @@
-// first commit
+terraform {
+  required_providers {
+    hcp = {
+      source = "hashicorp/hcp"
+      version = "0.60.0"
+    }
+    doormat = {
+      source  = "doormat.hashicorp.services/hashicorp-security/doormat"
+      version = "~> 0.0.2"
+    }
+  }
+}
+
+provider "doormat" {}
+
+data "doormat_aws_credentials" "creds" {
+  provider = doormat
+  role_arn = "arn:aws:iam::365006510262:role/tfc-doormat-role"
+}
+
