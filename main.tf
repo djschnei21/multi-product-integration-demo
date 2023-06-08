eb732b7960d89d2cd8f67e5ffa6d6b348cbd86c0
djschnei21 <32646250+djschnei21@users.noreply.github.com>
2023-06-08 10:52:17 -0400

    output

--- main.tf
+++ main.tf
@@ -29,3 +29,21 @@ resource "hcp_consul_cluster" "hashistack" {
   hvn_id     = hcp_hvn.hashistack.id
   tier       = "development"
 }
+
+resource "hcp_boundary_cluster" "hashistack" {
+  cluster_id = "boundary-cluster"
+  username   = "admin"
+  password   = var.boundary_password
+}
+
+output "vault_url" {
+  value = hcp_vault_cluster.hashistack.public_endpoint
+}
+
+output "consul_url" {
+  value = hcp_consul_cluster.hashistack.consul_public_endpoint_url
+}
+
+output "boundary_url" {
+  value = hcp_boundary_cluster.hashistack.cluster_url
+}
