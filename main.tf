c1ca1a95e17f102e5d5d08d689dee1e0886c26d6
djschnei21 <32646250+djschnei21@users.noreply.github.com>
2023-07-14 15:37:56 -0400

    move provider configs to root module

--- main.tf
+++ main.tf
@@ -13,9 +13,8 @@ terraform {
 
 provider "doormat" {}
 
-data "doormat_aws_credentials" "creds" {
-  provider = doormat
-  role_arn = "arn:aws:iam::365006510262:role/tfc-doormat-role"
+provider "hcp" {
+    project_id = var.project_id
 }
 
 provider "aws" {
@@ -23,6 +22,11 @@ provider "aws" {
   access_key = data.doormat_aws_credentials.creds.access_key
   secret_key = data.doormat_aws_credentials.creds.secret_key
   token      = data.doormat_aws_credentials.creds.token
+}
+
+data "doormat_aws_credentials" "creds" {
+  provider = doormat
+  role_arn = "arn:aws:iam::365006510262:role/tfc-doormat-role"
 }
 
 module "aws_landing_zone" {
