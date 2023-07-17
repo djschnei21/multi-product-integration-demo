fd773c759c3281f48d035330d7992e782100887e
djschnei21 <32646250+djschnei21@users.noreply.github.com>
2023-07-17 08:19:03 -0400

    temp comment out these outputs

--- outputs.tf
+++ outputs.tf
@@ -10,24 +10,24 @@ output "hcp_hvn_id" {
   value = module.hcp_hvn_aws.hcp_hvn_id
 }
 
-output "vault_url" {
-  value = module.hcp_clusters.vault_url
-}
+# output "vault_url" {
+#   value = module.hcp_clusters.vault_url
+# }
 
-output "vault_admin_token" {
-  sensitive = true
-  value     = module.hcp_clusters.vault_admin_token
-}
+# output "vault_admin_token" {
+#   sensitive = true
+#   value     = module.hcp_clusters.vault_admin_token
+# }
 
-output "consul_url" {
-  value = module.hcp_clusters.consul_url
-}
+# output "consul_url" {
+#   value = module.hcp_clusters.consul_url
+# }
 
-output "consul_admin_token" {
-  sensitive = true
-  value     = module.hcp_clusters.consul_admin_token
-}
+# output "consul_admin_token" {
+#   sensitive = true
+#   value     = module.hcp_clusters.consul_admin_token
+# }
 
-output "boundary_url" {
-  value = module.hcp_clusters.boundary_url
-}
+# output "boundary_url" {
+#   value = module.hcp_clusters.boundary_url
+# }
