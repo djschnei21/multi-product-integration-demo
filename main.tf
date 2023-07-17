e0fa7b8b4ec97d9ddb229fecf3d3ed1dc7edb31b
djschnei21 <32646250+djschnei21@users.noreply.github.com>
2023-07-17 08:29:40 -0400

    new approach

--- main.tf
+++ main.tf
@@ -33,25 +33,3 @@ data "doormat_aws_credentials" "creds" {
   role_arn = "arn:aws:iam::365006510262:role/tfc-doormat-role"
 }
 
-module "aws_landing_zone" {
-  source     = "./modules/csp-landing-zones/aws"
-  stack_name = var.stack_name
-}
-
-# module "hcp_hvn_aws" {
-#   depends_on = [module.aws_landing_zone] #required because dependency on default SG being created isnt implicit
-#   source     = "./modules/hcp-control-plane/hashicorp-virtual-network"
-#   stack_name = var.stack_name
-#   aws_vpc_id = module.aws_landing_zone.vpc_id
-# }
-
-# module "hcp_clusters" {
-#   source                  = "./modules/hcp-control-plane/clusters"
-#   stack_name              = var.stack_name
-#   hcp_hvn_id              = module.hcp_hvn_aws.hcp_hvn_id
-#   boundary_cluster_tier   = var.boundary_cluster_tier
-#   boundary_admin_username = var.boundary_admin_username
-#   boundary_admin_password = var.boundary_admin_password
-#   vault_cluster_tier      = var.vault_cluster_tier
-#   consul_cluster_tier     = var.consul_cluster_tier
-# }
