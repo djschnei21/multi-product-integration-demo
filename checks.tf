744611bd0608b917a2e7781c04dee5eb4e70e272
djschnei21 <32646250+djschnei21@users.noreply.github.com>
2023-07-17 08:33:00 -0400

    remove all

--- checks.tf
+++ checks.tf
@@ -1,32 +1,32 @@
-check "vault_health_check" {
-  data "http" "vault_cluster" {
-    url = "${module.hcp_clusters.vault_url}/ui/"
-  }
+# check "vault_health_check" {
+#   data "http" "vault_cluster" {
+#     url = "${module.hcp_clusters.vault_url}/ui/"
+#   }
 
-  assert {
-    condition     = data.http.vault_cluster.status_code == 200
-    error_message = "${var.stack_name}-vault-cluster returned an unhealthy status code"
-  }
-}
+#   assert {
+#     condition     = data.http.vault_cluster.status_code == 200
+#     error_message = "${var.stack_name}-vault-cluster returned an unhealthy status code"
+#   }
+# }
 
-check "consul_health_check" {
-  data "http" "consul_cluster" {
-    url = module.hcp_clusters.consul_url
-  }
+# check "consul_health_check" {
+#   data "http" "consul_cluster" {
+#     url = module.hcp_clusters.consul_url
+#   }
 
-  assert {
-    condition     = data.http.consul_cluster.status_code == 200
-    error_message = "${var.stack_name}-consul-cluster returned an unhealthy status code"
-  }
-}
+#   assert {
+#     condition     = data.http.consul_cluster.status_code == 200
+#     error_message = "${var.stack_name}-consul-cluster returned an unhealthy status code"
+#   }
+# }
 
-check "boundary_chealth_check" {
-  data "http" "boundary_cluster" {
-    url = module.hcp_clusters.boundary_url
-  }
+# check "boundary_chealth_check" {
+#   data "http" "boundary_cluster" {
+#     url = module.hcp_clusters.boundary_url
+#   }
 
-  assert {
-    condition     = data.http.boundary_cluster.status_code == 200
-    error_message = "${var.stack_name}-boundary-cluster returned an unhealthy status code"
-  }
-}
+#   assert {
+#     condition     = data.http.boundary_cluster.status_code == 200
+#     error_message = "${var.stack_name}-boundary-cluster returned an unhealthy status code"
+#   }
+# }
--- main.tf
+++ main.tf
@@ -33,3 +33,38 @@ data "doormat_aws_credentials" "creds" {
   role_arn = "arn:aws:iam::365006510262:role/tfc-doormat-role"
 }
 
+data "aws_availability_zones" "available" {
+  filter {
+    name   = "zone-type"
+    values = ["availability-zone"]
+  }
+}
+
+# module "vpc" {
+#   source  = "terraform-aws-modules/vpc/aws"
+#   version = "3.10.0"
+
+#   azs                  = data.aws_availability_zones.available.names
+#   cidr                 = "10.0.0.0/16"
+#   enable_dns_hostnames = true
+#   name                 = "${var.cluster_id}-vpc"
+#   private_subnets      = []
+#   public_subnets       = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
+# }
+
+# resource "hcp_hvn" "main" {
+#   hvn_id         = var.hvn_id
+#   cloud_provider = "aws"
+#   region         = var.hvn_region
+#   cidr_block     = var.hvn_cidr_block
+# }
+
+# module "aws_hcp_consul" {
+#   source  = "hashicorp/hcp-consul/aws"
+#   version = "~> 0.12.1"
+
+#   hvn             = hcp_hvn.main
+#   vpc_id          = module.vpc.vpc_id
+#   subnet_ids      = module.vpc.public_subnets
+#   route_table_ids = module.vpc.public_route_table_ids
+# }
