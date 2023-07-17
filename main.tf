82549e1db4abdb82da1fc01bfb540d84934d9764
djschnei21 <32646250+djschnei21@users.noreply.github.com>
2023-07-17 14:19:14 -0400

    pull image metadata from HCP

--- main.tf
+++ main.tf
@@ -95,38 +95,13 @@ resource "hcp_boundary_cluster" "hashistack" {
   password   = var.boundary_admin_password
 }
 
-# resource "tls_private_key" "ssh" {
-#   algorithm = "RSA"
-#   rsa_bits  = 4096
-# }
+data "hcp_packer_image" "ubuntu-lunar-hashi" {
+  bucket_name     = "ubuntu-lunar-hashi"
+  channel         = "latest"
+  cloud_provider  = "aws"
+  region          = "us-east-2"
+}
 
-# resource "aws_key_pair" "hcp_ec2" {
-#   public_key = tls_private_key.ssh.public_key_openssh
-#   key_name   = "hcp-ec2-key-${var.stack_id}"
-# }
-
-# resource "local_file" "ssh_key" {
-#   content         = tls_private_key.ssh.private_key_pem
-#   file_permission = "400"
-#   filename        = "${path.module}/${aws_key_pair.hcp_ec2.key_name}.pem"
-# }
-
-# module "aws_ec2_consul_client" {
-#   source  = "hashicorp/hcp-consul/aws//modules/hcp-ec2-client"
-#   version = "~> 0.12.1"
-
-#   allowed_http_cidr_blocks = ["0.0.0.0/0"]
-#   allowed_ssh_cidr_blocks  = ["0.0.0.0/0"]
-#   client_ca_file           = hcp_consul_cluster.hashistack.consul_ca_file
-#   client_config_file       = hcp_consul_cluster.hashistack.consul_config_file
-#   consul_version           = hcp_consul_cluster.hashistack.consul_version
-#   nat_public_ips           = module.vpc.nat_public_ips
-#   install_demo_app         = var.install_demo_app
-#   root_token               = hcp_consul_cluster_root_token.token.secret_id
-#   security_group_id        = module.aws_hcp_network_config.security_group_id
-#   ssh_key                  = tls_private_key.ssh.private_key_pem
-#   ssh_keyname              = aws_key_pair.hcp_ec2.key_name
-#   ssm                      = var.ssm
-#   subnet_id                = module.vpc.public_subnets[0]
-#   vpc_id                   = module.vpc.vpc_id
-# }
+output "image" {
+  value = data.hcp_packer_image.ubuntu-lunar-hashi
+}
