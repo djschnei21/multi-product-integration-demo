fa3bf947ecdf20870687537cb5dd54eb9a93eb99
djschnei21 <32646250+djschnei21@users.noreply.github.com>
2023-07-17 09:13:15 -0400

    remove outputs for now

--- outputs.tf
+++ outputs.tf
@@ -1,12 +1,12 @@
-output "consul_root_token" {
-  value     = hcp_consul_cluster_root_token.token.secret_id
-  sensitive = true
-}
+# output "consul_root_token" {
+#   value     = hcp_consul_cluster_root_token.token.secret_id
+#   sensitive = true
+# }
 
-output "consul_url" {
-  value = hcp_consul_cluster.main.public_endpoint ? (
-    hcp_consul_cluster.main.consul_public_endpoint_url
-    ) : (
-    hcp_consul_cluster.main.consul_private_endpoint_url
-  )
-}
+# output "consul_url" {
+#   value = hcp_consul_cluster.main.public_endpoint ? (
+#     hcp_consul_cluster.main.consul_public_endpoint_url
+#     ) : (
+#     hcp_consul_cluster.main.consul_private_endpoint_url
+#   )
+# }
