e9e860efba76a3c3596a9a3dc457de9df5b8c306
djschnei21 <32646250+djschnei21@users.noreply.github.com>
2023-07-17 10:02:52 -0400

    variable descriptions

--- variables.tf
+++ variables.tf
@@ -42,21 +42,27 @@ variable "hvn_cidr_block" {
 
 variable "boundary_admin_username" {
   type = string
+  description = "The admin username to be created on the Boundary cluster"
+  default     = "admin"
 }
 
 variable "boundary_admin_password" {
   type      = string
+  description = "The admin user's password on the Boundary cluster"
   sensitive = true
 }
 
 variable "boundary_cluster_tier" {
   type = string
+  description = "The tier used when creating the Boundary cluster"
 }
 
 variable "vault_cluster_tier" {
   type = string
+  description = "The tier used when creating the Vault cluster"
 }
 
 variable "consul_cluster_tier" {
   type = string
+  description = "The tier used when creating the Consul cluster"
 }
