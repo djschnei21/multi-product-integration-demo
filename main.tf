4fd4793ddc0239422eaf2a359646b5015b9070ec
djschnei21 <32646250+djschnei21@users.noreply.github.com>
2023-07-17 18:20:49 -0400

    allow nomad servers to talk

--- main.tf
+++ main.tf
@@ -106,6 +106,13 @@ resource "aws_security_group" "nomad_server" {
     cidr_blocks = ["0.0.0.0/0"]
   }
 
+  ingress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    self        = true  # reference to the security group itself
+  }
+
   egress {
     from_port   = 0
     to_port     = 0
