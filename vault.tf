0edc933e85eebb33c352fedce7786eae4ea7be88
djschnei21 <32646250+djschnei21@users.noreply.github.com>
2023-07-18 17:01:56 -0400

    JASON

--- vault.tf
+++ vault.tf
@@ -17,6 +17,6 @@ data "vault_kv_secret_v2" "bootstrap" {
 }
 
 output "bootstrap" {
-  value     = data.vault_kv_secret_v2.bootstrap.data["SecretID"]
+  value     = jsonencode(data.vault_kv_secret_v2.bootstrap.data)["SecretID"]
   sensitive = true
 }
