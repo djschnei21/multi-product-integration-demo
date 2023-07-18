133abc00c76ce10e3fa9d208f1921917a28743d2
djschnei21 <32646250+djschnei21@users.noreply.github.com>
2023-07-18 15:16:51 -0400

    namespace

--- vault.tf
+++ vault.tf
@@ -1,6 +1,7 @@
 provider "vault" {
   address = hcp_vault_cluster.hashistack.public_endpoint
   token = hcp_vault_cluster_admin_token.provider.token
+  namespace = "admin/"
 }
 
 resource "vault_mount" "kvv2" {
