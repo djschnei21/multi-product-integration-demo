fb42233950cf6b77f02ce63b5378bda85312e58f
djschnei21 <32646250+djschnei21@users.noreply.github.com>
2023-07-18 10:15:52 -0400

    listener

--- aws.tf
+++ aws.tf
@@ -74,7 +74,7 @@ resource "aws_security_group" "nomad_lb" {
   }
 }
 
-resource "aws_alb" "main" {
+resource "aws_alb" "nomad" {
   name               = "nomad-alb"
   security_groups    = [ aws_security_group.nomad_lb.id ]
   subnets            = module.vpc.public_subnets
@@ -89,6 +89,17 @@ resource "aws_alb_target_group" "nomad" {
   health_check {
     path = "/v1/status/leader"
     port = "4646"
+  }
+}
+
+resource "aws_alb_listener" "nomad" {
+  load_balancer_arn = aws_alb.nomad.arn
+  port              = "80"
+  protocol          = "HTTP"
+
+  default_action {
+    type             = "forward"
+    target_group_arn = aws_alb_target_group.nomad.arn
   }
 }
 
