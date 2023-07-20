0bea016b29607e22f307a64def5fb17e48f17f49
djschnei21 <32646250+djschnei21@users.noreply.github.com>
2023-07-20 11:10:06 -0400

    all workspaces

--- main.tf
+++ main.tf
@@ -8,16 +8,58 @@ terraform {
 
 provider "tfe" {}
 
-# resource "tfe_workspace" "test" {
-#   name          = "networking"
-#   organization  = var.tfc_organization
-#   project_id    = var.tfc_project_id
+resource "tfe_workspace" "networking" {
+  name          = "networking"
+  organization  = var.tfc_organization
+  project_id    = var.tfc_project_id
 
-#   vcs_repo {
-#     identifier = var.repo_identifier
-#     oauth_token_id = var.oauth_token_id
-#   }
+  vcs_repo {
+    identifier = var.repo_identifier
+    oauth_token_id = var.oauth_token_id
+  }
 
-#   working_directory = "networking"
-#   queue_all_runs = false
-# }
+  working_directory = "networking"
+  queue_all_runs = false
+}
+
+resource "tfe_workspace" "hcp_clusters" {
+  name          = "hcp-clusters"
+  organization  = var.tfc_organization
+  project_id    = var.tfc_project_id
+
+  vcs_repo {
+    identifier = var.repo_identifier
+    oauth_token_id = var.oauth_token_id
+  }
+
+  working_directory = "hcp-clusters"
+  queue_all_runs = false
+}
+
+resource "tfe_workspace" "nomad_cluster" {
+  name          = "nomad-cluster"
+  organization  = var.tfc_organization
+  project_id    = var.tfc_project_id
+
+  vcs_repo {
+    identifier = var.repo_identifier
+    oauth_token_id = var.oauth_token_id
+  }
+
+  working_directory = "nomad-cluster"
+  queue_all_runs = false
+}
+
+resource "tfe_workspace" "nomad_nodes" {
+  name          = "nomad-nodes"
+  organization  = var.tfc_organization
+  project_id    = var.tfc_project_id
+
+  vcs_repo {
+    identifier = var.repo_identifier
+    oauth_token_id = var.oauth_token_id
+  }
+
+  working_directory = "nomad-nodes"
+  queue_all_runs = false
+}
