caf868846b9afe5da5d5f5a173b5c2b016166fa7
djschnei21 <32646250+djschnei21@users.noreply.github.com>
2023-07-20 09:31:20 -0400

    fixes

--- nomad-cluster/outputs.tf
+++ nomad-cluster/outputs.tf
@@ -4,24 +4,24 @@ output "nomad_sg" {
 
 # Passthrough outputs to enable cascading plans
 output "consul_root_token" {
-  value = data.terraform_remote_state.hcp_clusters.provider.secret_id
+  value = data.terraform_remote_state.hcp_clusters.outputs.consul_root_token
   sensitive = true
 }
 
 output "consul_ca_file" {
-  value = data.terraform_remote_state.hcp_clusters.consul_ca_file
+  value = data.terraform_remote_state.hcp_clusters.outputs.consul_ca_file
   sensitive = true
 }
 
 output "consul_config_file" {
-  value = data.terraform_remote_state.hcp_clusters.consul_config_file
+  value = data.terraform_remote_state.hcp_clusters.outputs.consul_config_file
   sensitive = true
 }
 
 output "subnet_ids" {
-  value = data.terraform_remote_state.hcp_clusters.subnet_ids
+  value = data.terraform_remote_state.hcp_clusters.outputs.subnet_ids
 }
 
 output "hvn_sg_id" {
-  value = data.terraform_remote_state.hcp_clusters.hvn_sg_id
+  value = data.terraform_remote_state.hcp_clusters.outputs.hvn_sg_id
 }
