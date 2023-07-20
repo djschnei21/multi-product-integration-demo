9c90df9cab36f95f610e0a4d51c156213e804a03
djschnei21 <32646250+djschnei21@users.noreply.github.com>
2023-07-20 15:30:14 -0400

    increase node count

--- nomad-nodes/main.tf
+++ nomad-nodes/main.tf
@@ -148,7 +148,7 @@ resource "aws_launch_template" "nomad_client_x86_launch_template" {
 }
 
 resource "aws_autoscaling_group" "nomad_client_x86_asg" {
-  desired_capacity  = 2
+  desired_capacity  = 3
   max_size          = 5
   min_size          = 1
   health_check_type = "EC2"
@@ -210,7 +210,7 @@ resource "aws_launch_template" "nomad_client_arm_launch_template" {
 }
 
 resource "aws_autoscaling_group" "nomad_client_arm_asg" {
-  desired_capacity  = 2
+  desired_capacity  = 3
   max_size          = 5
   min_size          = 1
   health_check_type = "EC2"
