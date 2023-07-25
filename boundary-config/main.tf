d232b4a78b88800a15bfd032c47710d0a60d43fd
djschnei21 <32646250+djschnei21@users.noreply.github.com>
2023-07-25 20:16:02 +0000

    vault inject

--- boundary-config/main.tf
+++ boundary-config/main.tf
@@ -14,6 +14,11 @@ terraform {
       source = "hashicorp/boundary"
       version = "~> 1.1.9"
     }
+
+    vault = {
+      source = "hashicorp/vault"
+      version = "~> 3.18.0"
+    }
   }
 }
 
@@ -40,6 +45,12 @@ data "terraform_remote_state" "hcp_clusters" {
       name = "hcp-clusters"
     }
   }
+}
+
+provider "vault" {
+  address = data.terraform_remote_state.hcp_clusters.outputs.vault_public_endpoint
+  token = data.terraform_remote_state.hcp_clusters.outputs.vault_root_token
+  namespace = "admin"
 }
 
 provider "boundary" {
@@ -102,6 +113,71 @@ resource "boundary_host_catalog_plugin" "aws" {
     "access_key_id"     = "${aws_iam_access_key.boundary.id}",
     "secret_access_key" = "${aws_iam_access_key.boundary.secret}"
   })
+}
+
+resource "vault_policy" "boundary_controller" {
+  name = "boundary-controller"
+
+  policy = <<EOT
+path "auth/token/lookup-self" {
+  capabilities = ["read"]
+}
+
+path "auth/token/renew-self" {
+  capabilities = ["update"]
+}
+
+path "auth/token/revoke-self" {
+  capabilities = ["update"]
+}
+
+path "sys/leases/renew" {
+  capabilities = ["update"]
+}
+
+path "sys/leases/revoke" {
+  capabilities = ["update"]
+}
+
+path "sys/capabilities-self" {
+  capabilities = ["update"]
+}
+
+path "ssh/boundary_role" {
+  capabilities = ["read"]
+}
+EOT
+}
+
+resource "vault_token" "boundary_controller" {
+  role_name = "boundary_controller"
+
+  policies = [vault_policy.boundary_controller.name]
+
+  renewable = true
+  ttl = "24h"
+
+  renew_min_lease = 43200
+  renew_increment = 86400
+
+  metadata = {
+    "purpose" = "service-account"
+  }
+}
+
+resource "boundary_credential_store_vault" "vault" {
+  name        = "foo"
+  description = "My first Vault credential store!"
+  address     = data.terraform_remote_state.hcp_clusters.outputs.vault_public_endpoint
+  token       = vault_token.boundary_controller.client_token 
+  scope_id    = boundary_scope.project.id
+}
+
+resource "boundary_credential_library_vault_ssh_certificate" "vault" {
+  name                = "vault"
+  credential_store_id = boundary_credential_store_vault.vault.id
+  path                = "ssh/sign/boundary"
+  username            = "ubuntu"
 }
 
 resource "boundary_host_set_plugin" "nomad_servers" {
