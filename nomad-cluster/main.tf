c94b4a682180379fe5064035f3d5b9761a31c1f7
djschnei21 <32646250+djschnei21@users.noreply.github.com>
2023-07-25 18:46:27 +0000

    allow 22 inbound

--- nomad-cluster/main.tf
+++ nomad-cluster/main.tf
@@ -75,6 +75,13 @@ resource "aws_security_group" "nomad" {
     self        = true  # reference to the security group itself
   }
 
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cird_blocks = ["0.0.0.0/0"]
+  }
+
   egress {
     from_port   = 0
     to_port     = 0
