d8d0c53ecb4b17aa9116640ffca097ed30f6f1f4
djschnei21 <32646250+djschnei21@users.noreply.github.com>
2023-08-22 14:23:30 -0400

    allow inbound on 27017 for mongodb

--- nomad-cluster/main.tf
+++ nomad-cluster/main.tf
@@ -107,6 +107,13 @@ resource "aws_security_group" "nomad" {
     cidr_blocks = ["0.0.0.0/0"]
   }
 
+  ingress {
+    from_port   = 27017
+    to_port     = 27017
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
   egress {
     from_port   = 0
     to_port     = 0
