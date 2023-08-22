de979da57591f87a05fd81529d238d2c7c6aa903
djschnei21 <32646250+djschnei21@users.noreply.github.com>
2023-08-22 13:38:23 -0400

    pass in the nomad token

--- workload/wait-for-nomad-job.sh
+++ workload/wait-for-nomad-job.sh
@@ -3,14 +3,15 @@
 set -e
 
 if [ -z "$1" ]; then
-  echo "Usage: $0 <job-id>"
+  echo "Usage: $0 <job-id> [<nomad-addr>] [<nomad-token>]"
   exit 1
 fi
 
 JOB_ID="$1"
-NOMAD_ADDR="${2:-http://nomad.service.consul:4646}"
+NOMAD_ADDR="${2}"
+NOMAD_TOKEN="${3}"
 
-until [ "$(curl -s --fail "${NOMAD_ADDR}/v1/job/${JOB_ID}/allocations" | jq -r '.[].ClientStatus' | grep -c "running")" -gt 0 ]; do
+until [ "$(curl -s --fail -H "X-Nomad-Token: ${NOMAD_TOKEN}" "${NOMAD_ADDR}/v1/job/${JOB_ID}/allocations" | jq -r '.[].ClientStatus' | grep -c "running")" -gt 0 ]; do
   echo "Waiting for job ${JOB_ID} to become healthy..."
   sleep 5
 done
