4f8cb650ede3cb67ae53a0ef94733576dec05764
djschnei21 <32646250+djschnei21@users.noreply.github.com>
2023-08-22 14:57:41 -0400

    corrected role

--- boundary-config/main.tf
+++ boundary-config/main.tf
@@ -26,7 +26,7 @@ provider "doormat" {}
 
 data "doormat_aws_credentials" "creds" {
   provider = doormat
-  role_arn = "arn:aws:iam::365006510262:role/tfc-doormat-role_boundary-config"
+  role_arn = "arn:aws:iam::365006510262:role/tfc-doormat-role_5_boundary-config"
 }
 
 provider "aws" {
