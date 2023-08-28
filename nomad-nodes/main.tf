8f9a82120e942d5d2d3a42620c7038da882847a1
djschnei21 <32646250+djschnei21@users.noreply.github.com>
2023-08-28 11:35:05 -0400

    port 80 for now

--- nomad-nodes/main.tf
+++ nomad-nodes/main.tf
@@ -245,8 +245,8 @@ resource "aws_security_group" "nomad_client_alb" {
   vpc_id = data.terraform_remote_state.networking.outputs.vpc_id
 
   ingress {
-    from_port   = 443
-    to_port     = 443
+    from_port   = 80
+    to_port     = 80
     protocol    = "tcp"
     cidr_blocks = [ "0.0.0.0/0" ]
   }
