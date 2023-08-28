18a2efe4ed4d91e45c456fe28e730d6bda617c8d
djschnei21 <32646250+djschnei21@users.noreply.github.com>
2023-08-28 11:43:47 -0400

    output asg arns

--- nomad-nodes/outputs.tf
+++ nomad-nodes/outputs.tf
@@ -1,3 +1,11 @@
 output "nomad_client_alb" {
   value = aws_lb.nomad_clients.id
 }
+
+output "nomad_client_x86_asg" {
+  value = aws_autoscaling_group.nomad_client_x86_asg.arn
+}
+
+output "nomad_client_arm_asg" {
+  value = aws_autoscaling_group.nomad_client_arm_asg.arn
+}
