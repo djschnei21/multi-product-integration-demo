dd4688dfdd0b4d0a892dffd8fb1470dd2e20d2e8
djschnei21 <32646250+djschnei21@users.noreply.github.com>
2023-08-30 07:31:03 -0400

    allow inbound to the dashboard service

--- nomad-cluster/main.tf
+++ nomad-cluster/main.tf
@@ -140,6 +140,13 @@ resource "aws_security_group" "nomad" {
     cidr_blocks = ["0.0.0.0/0"]
   }
 
+  ingress {
+    from_port   = 3100
+    to_port     = 3100
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
   egress {
     from_port   = 0
     to_port     = 0
