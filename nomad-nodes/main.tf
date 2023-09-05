8dbf63ed682f93008d6cf90701214d4ce23f517b
djschnei21 <32646250+djschnei21@users.noreply.github.com>
2023-09-05 13:39:55 -0400

    create nomad node pools by setting the config name

--- nomad-nodes/main.tf
+++ nomad-nodes/main.tf
@@ -173,7 +173,7 @@ resource "aws_launch_template" "nomad_client_x86_launch_template" {
         consul_ca_file     = data.terraform_remote_state.hcp_clusters.outputs.consul_ca_file,
         consul_config_file = data.terraform_remote_state.hcp_clusters.outputs.consul_config_file,
         consul_acl_token   = data.terraform_remote_state.hcp_clusters.outputs.consul_root_token,
-        node_pool          = nomad_node_pool.x86.name,
+        node_pool          = "x86",
         vault_ssh_pub_key  = data.terraform_remote_state.nomad_cluster.outputs.ssh_ca_pub_key,
         vault_public_endpoint = data.terraform_remote_state.hcp_clusters.outputs.vault_public_endpoint
       }
@@ -241,7 +241,7 @@ resource "aws_launch_template" "nomad_client_arm_launch_template" {
         consul_ca_file     = data.terraform_remote_state.hcp_clusters.outputs.consul_ca_file,
         consul_config_file = data.terraform_remote_state.hcp_clusters.outputs.consul_config_file,
         consul_acl_token   = data.terraform_remote_state.hcp_clusters.outputs.consul_root_token,
-        node_pool          = nomad_node_pool.arm.name,
+        node_pool          = "arm",
         vault_ssh_pub_key  = data.terraform_remote_state.nomad_cluster.outputs.ssh_ca_pub_key,
         vault_public_endpoint = data.terraform_remote_state.hcp_clusters.outputs.vault_public_endpoint
       }
