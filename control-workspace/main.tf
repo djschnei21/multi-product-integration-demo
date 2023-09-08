21b8a3a36d67077f291750f5888623540b5680c5
djschnei21 <32646250+djschnei21@users.noreply.github.com>
2023-09-08 13:58:43 -0400

    remove workload workspace

--- control-workspace/main.tf
+++ control-workspace/main.tf
@@ -104,22 +104,6 @@ resource "tfe_workspace" "nomad_nodes" {
   remote_state_consumer_ids = [ tfe_workspace.workload.id ]
 }
 
-resource "tfe_workspace" "workload" {
-  name          = "7_workload"
-  organization  = var.tfc_organization
-  project_id    = var.tfc_project_id
-
-  vcs_repo {
-    identifier = var.repo_identifier
-    oauth_token_id = var.oauth_token_id
-  }
-
-  working_directory = "workload"
-  queue_all_runs = false
-  assessments_enabled = false
-  remote_state_consumer_ids = [ ]
-}
-
 resource "tfe_workspace_run" "networking" {
   workspace_id    = tfe_workspace.networking.id
 
@@ -219,18 +203,6 @@ resource "tfe_workspace_run" "nomad_nodes" {
     retry_attempts    = 5
     retry_backoff_min = 5
   }
-  destroy {
-    manual_confirm    = false
-    wait_for_run      = true
-    retry_attempts    = 5
-    retry_backoff_min = 5
-  }
-}
-
-resource "tfe_workspace_run" "workload" {
-  depends_on = [ tfe_workspace_run.nomad_nodes ]
-  workspace_id    = tfe_workspace.workload.id
-
   destroy {
     manual_confirm    = false
     wait_for_run      = true
