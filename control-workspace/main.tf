143811daf94e933c68e36fa1d96d9f3d6d8b0bef
djschnei21 <32646250+djschnei21@users.noreply.github.com>
2023-10-24 11:41:19 -0400

    will deploy the workload with a no-code module

--- control-workspace/main.tf
+++ control-workspace/main.tf
@@ -105,21 +105,21 @@ resource "tfe_workspace" "nomad_nodes" {
 }
 
 
-resource "tfe_workspace" "workload" {
-  name          = "7_workload"
-  organization  = var.tfc_organization
-  project_id    = var.tfc_project_id
+# resource "tfe_workspace" "workload" {
+#   name          = "7_workload"
+#   organization  = var.tfc_organization
+#   project_id    = var.tfc_project_id
 
-  vcs_repo {
-    identifier = var.repo_identifier
-    oauth_token_id = var.oauth_token_id
-  }
+#   vcs_repo {
+#     identifier = var.repo_identifier
+#     oauth_token_id = var.oauth_token_id
+#   }
 
-  working_directory = "workload"
-  queue_all_runs = false
-  assessments_enabled = false
-  global_remote_state = true
-}
+#   working_directory = "workload"
+#   queue_all_runs = false
+#   assessments_enabled = false
+#   global_remote_state = true
+# }
 
 resource "tfe_workspace_run" "networking" {
   workspace_id    = tfe_workspace.networking.id
