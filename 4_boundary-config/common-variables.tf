fc73f8fbadfd6b052710a325617dffe107a12af6
djschnei21 <32646250+djschnei21@users.noreply.github.com>
2024-04-15 12:39:35 -0400

    remove dupes

--- 4_boundary-config/common-variables.tf
+++ 4_boundary-config/common-variables.tf
@@ -1,28 +1,5 @@
-variable "aws_account_id" {
-  type = string
-}
-
-variable "boundary_admin_password" {
-  type = string
-}
-
-variable "my_email" {
-  type = string
-}
-
 variable "nomad_license" {
   type = string
 }
 
-variable "region" {
-  type = string
-}
-
-variable "stack_id" {
-  type = string
-}
-
-variable "tfc_organization" {
-  type = string
-}
 
