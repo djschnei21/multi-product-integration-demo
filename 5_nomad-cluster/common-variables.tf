f3be6ca365a9f3be904256afc1e29fbb6a2663ac
djschnei21 <32646250+djschnei21@users.noreply.github.com>
2024-04-15 12:46:01 -0400

    remove dupes

--- 5_nomad-cluster/common-variables.tf
+++ 5_nomad-cluster/common-variables.tf
@@ -1,7 +1,3 @@
-variable "aws_account_id" {
-  type = string
-}
-
 variable "boundary_admin_password" {
   type = string
 }
@@ -10,19 +6,6 @@ variable "my_email" {
   type = string
 }
 
-variable "nomad_license" {
+variable "auth_method" {
   type = string
 }
-
-variable "region" {
-  type = string
-}
-
-variable "stack_id" {
-  type = string
-}
-
-variable "tfc_organization" {
-  type = string
-}
-
