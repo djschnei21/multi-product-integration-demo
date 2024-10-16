1b0383fac5b8d70706dd15fffee43a03afed1c60
Dan Schneider <djschnei21@gmail.com>
2024-10-16 15:53:47 -0400

    ephemeral

--- 2_hcp-clusters/variables.tf
+++ 2_hcp-clusters/variables.tf
@@ -31,5 +31,4 @@ variable "boundary_admin_username" {
 variable "boundary_admin_password" {
   type        = string
   description = "The admin user's password on the Boundary cluster"
-  sensitive   = true
 }
